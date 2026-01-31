3c7e1eba211308e43668733e79eed3bdc447b330
Francesco Vinciguerra <francesco.vinciguerra@epfl.ch>
2026-01-31 17:10:40 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
-content: [
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Questa riga è fondamentale!
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#D4AF37',
+        luxury: '#2D0A0A',
+      },
+    },
+  },
+  plugins: [],
+}
