f4efac10755989c2b28872be674cf02e753dff88
Francesco Vinciguerra <francesco.vinciguerra@epfl.ch>
2026-01-31 16:10:29 +0100

    Update ChatCliente.js

--- app/[restaurantId]/ChatCliente.js
+++ app/[restaurantId]/ChatCliente.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useTransition } from 'react';
-import { handleConsiglioAction } from '@/app/actions/chat';
+import { handleConsiglioAction } from '../actions/chat';
 
 export default function ChatCliente({ menu, restaurantId }) {
   const [selectedDishes, setSelectedDishes] = useState([]);
